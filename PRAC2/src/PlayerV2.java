b56f518318d5cfac00addb62b1622a05b0e0d47f
jacomof <joaquinfigueira85@gmail.com>
2022-04-12 02:12:47 +0200

    Ejercicio 4 a medias

---
+++ PRAC2/src/PlayerV2.java
@@ -0,0 +1,129 @@
+
+package sid.prac2;
+
+import java.util.*;
+import bdi4jade.goal.Goal;
+import bdi4jade.plan.DefaultPlan;
+import bdi4jade.plan.Plan.EndState;
+//import bdi4jade.plan.planbody.AbstractPlanBody;
+import bdi4jade.plan.planbody.*;
+import bdi4jade.plan.Plan;
+import bdi4jade.belief.*;
+import bdi4jade.core.*;
+import bdi4jade.goal.*;
+
+public class PlayerV2 extends SingleCapabilityAgent {
+  private int CC, CD, DC, DD;
+  
+  protected void init() {
+    Object[] args = getArguments();
+    if (args.length != 4) {
+      System.out.println("Wrong number of parameters for player inicialization. Arguments provided" +
+              args.length + "expected four.");
+      doDelete();
+    }
+
+    int CC = Integer.parseInt(args[0].toString());
+    int CD = Integer.parseInt(args[1].toString());
+    int DC = Integer.parseInt(args[2].toString());
+    int DD = Integer.parseInt(args[3].toString());
+    Belief C = new TransientBelief("C", new int[]{CC, CD});
+    Belief D = new TransientBelief("D", new int[]{DC, DD});
+    Belief history = new TransientBeliefSet("history", new HashSet());
+
+    Capability cap = this.getCapability();
+    BeliefBase beliefBase = cap.getBeliefBase();
+    beliefBase.addBelief(C);
+    beliefBase.addBelief(D);
+    beliefBase.addBelief(history);
+    this.addGoal(new BeliefSetHasValueGoal("history", new int[2]));
+    GoalTemplate goalTemplate = GoalTemplateFactory.hasValueOfTypeInBeliefSet("history", int[].class);
+    Plan plan = new DefaultPlan(goalTemplate,
+    notEmptyPlan.class);
+    getCapability().getPlanLibrary().addPlan(plan);
+    //System.out.println(this.getBeliefs());
+  }
+
+  public class minimizePlay implements Goal {
+    private static final String msg = "Hello World!";
+    
+    public String getText(){
+      return msg;
+    }
+  }
+  
+  public class minimizePlanBody extends AbstractPlanBody {
+  @Override
+    public void action() {
+    minimizePlay goal = (minimizePlay) getGoal();
+    BeliefBase bb = getBeliefBase();
+    System.out.println("A minimizar");
+    int[] c = (int[]) bb.getBelief("C").getValue();
+    int[] d = (int[]) bb.getBelief("D").getValue();
+    int minc = this.min(c);
+    int mind = this.min(d);
+    TransientBeliefSet hist = (TransientBeliefSet) bb.getBelief("history");
+    if(minc <= mind){
+      System.out.println(Arrays.toString(c));
+      hist.addValue(c);
+    }
+    else{
+      System.out.println(Arrays.toString(d));
+      hist.addValue(d);
+      
+    }
+    System.out.println("history es: ");
+    HashSet s = (HashSet) hist.getValue();
+    printHashSet(s);
+    setEndState(Plan.EndState.SUCCESSFUL);
+    }
+    
+    private int min(int[] a){
+      if(a[0] < a[1]) return a[0];
+      else return a[1];
+    }
+    
+    private void printHashSet(HashSet s){
+      for (Object v: s){
+        
+        int[] elem = (int[]) v; 
+        System.out.println(Arrays.toString(elem));
+      }
+    }
+    
+  }
+  public class notEmptyPlan extends BeliefGoalPlanBody {
+    @Override
+    protected void execute() {
+    
+      BeliefBase bb = getBeliefBase();
+      System.out.println("A minimizar");
+      int[] c = (int[]) bb.getBelief("C").getValue();
+      int[] d = (int[]) bb.getBelief("D").getValue();
+      int minc = this.min(c);
+      int mind = this.min(d);
+      TransientBeliefSet hist = (TransientBeliefSet) bb.getBelief("history");
+      if(minc <= mind){
+        System.out.println(Arrays.toString(c));
+        hist.addValue(c);
+      }
+      else{
+        System.out.println(Arrays.toString(d));
+        hist.addValue(d);
+        
+      }
+      
+    }
+      
+    private int min(int[] a){
+      if(a[0] < a[1]) return a[0];
+      else return a[1];
+    }
+      
+    
+  }
+  
+}
+
+
+
