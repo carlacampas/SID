fab2fb7a5840b367d29e58199291d0a5b730ccea
carlacampas <ccg.campas@gmail.com>
2022-06-11 19:48:40 +0200

    recolector moves better

--- PRAC/src/main/java/sid/prac/GeneralAgent.java
+++ PRAC/src/main/java/sid/prac/GeneralAgent.java
@@ -40,20 +40,21 @@ public class GeneralAgent extends AbstractDedaleAgent {
 	private AID brains;
 	Observation collectType;
 	
+
 	public MapRepresentation getMap() { return map; }
 	public void setup() {
 		super.setup();
-		
+
 		List<Behaviour> lb=new ArrayList<Behaviour>();
-		
+
 		lb.add(new OneShotBehaviour() {
 			public void action () {
 				System.out.println("one shot behaviour");
 				int sum = sumFreeSpace(getBackPackFreeSpace());
-				
+
 				if (sum == 0) {
 					type = "agentExplo";
-					
+
 					myAgent.addBehaviour(new ExploSoloBehaviour((AbstractDedaleAgent) this.myAgent, map));
 					addExplorerBrains();
 					System.out.println("I am an explorer!");
@@ -64,7 +65,7 @@ public class GeneralAgent extends AbstractDedaleAgent {
 					addRecolectorBrains();
 					System.out.println("I am a collector!");
 				}
-				
+
 				 // Registre al DF
 		        DFAgentDescription dfd = new DFAgentDescription();
 		        ServiceDescription sd = new ServiceDescription();
@@ -80,12 +81,12 @@ public class GeneralAgent extends AbstractDedaleAgent {
 		        }
 			}
 		});
-		
-		lb.add(new RecieveNextMove());	
+
+		lb.add(new RecieveNextMove());
 		addBehaviour(new startMyBehaviours(this,lb));
 		System.out.println("here, done with behaviour");
 	}
-	
+
 	public void addExplorerBrains() {
 		System.out.println("Llego a setup de Explorer");
 		AgentContainer ac = getContainerController();
@@ -94,32 +95,32 @@ public class GeneralAgent extends AbstractDedaleAgent {
 			AgentController ag = ac.createNewAgent("brainy_" + getName(), "sid.prac.ExplorerBrains", args);
 			ag.start();
 			brains = new AID(ag.getName(), AID.ISGUID);
-			
+
 		} catch (StaleProxyException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void addRecolectorBrains() {
 		System.out.println("RECOLECTOR PUEDE RECOGER: " + collectType.toString());
-		
+
 		final Object[] args = {getAID(), collectType, getCurrentPosition(), observe(), sumFreeSpace(getBackPackFreeSpace())};
-		
+
 		System.out.println("Llego a setup de Recolector");
-		
+
 		AgentContainer ac = getContainerController();
 		try {
 			AgentController ag = ac.createNewAgent("brainy_" + getName(), "sid.prac.RecolectorBrains", args);
 			ag.start();
 			brains = new AID(ag.getName(), AID.ISGUID);
-			
+
 		} catch (StaleProxyException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * This method is automatically called after doDelete()
 	 */
@@ -129,34 +130,34 @@ public class GeneralAgent extends AbstractDedaleAgent {
 	     } catch (FIPAException e) {}
 		super.takeDown();
 	}
-	
+
 	/**
-	 * This method is automatically called before migration. 
+	 * This method is automatically called before migration.
 	 * You can add here all the saving you need
 	 */
 	protected void beforeMove(){
 		super.beforeMove();
 	}
-	
+
 	/**
-	 * This method is automatically called after migration to reload. 
-	 * You can add here all the info regarding the state you want your agent to restart from 
-	 * 
+	 * This method is automatically called after migration to reload.
+	 * You can add here all the info regarding the state you want your agent to restart from
+	 *
 	 */
 	protected void afterMove(){
 		super.afterMove();
 	}
-	
+
 	public class RecieveNextMove extends CyclicBehaviour {
 		MessageTemplate tpl;
         ACLMessage msg;
-        
+
 		public void onStart() {
 			MessageTemplate tpl1 = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
 			MessageTemplate tpl2 = MessageTemplate.MatchSender(brains);
 			tpl = MessageTemplate.and(tpl1, tpl2);
         }
-		
+
 		public void handle_current_node (Couple<String, List<Couple<Observation, Integer>>> o) {
 			boolean myItem = false, lockOpen = false;
         	int strength = 0, lockPicking = 0;
@@ -181,10 +182,10 @@ public class GeneralAgent extends AbstractDedaleAgent {
 				}
 			}
 			if (!myItem) return;
-			
+
 			System.out.println("BEFORE: " + getBackPackFreeSpace());
 			Set<Couple<Observation, Integer>> exp = getMyExpertise();
-			
+
 			if (lockOpen) pick();
 			else {
 				boolean can_open = true;
@@ -192,14 +193,14 @@ public class GeneralAgent extends AbstractDedaleAgent {
 					if (e.equals(Observation.LOCKPICKING)) can_open = can_open && (e.getRight() >= lockPicking);
 					else if (e.equals(Observation.STRENGH)) can_open = can_open && (e.getRight() >= strength);
 				}
-				
+
 				if (!can_open) return;
 				openLock(Observation.ANY_TREASURE);
 				pick();
 			}
 			System.out.println("AFTER: " + getBackPackFreeSpace());
 		}
-		
+
         public void action() {
             msg = myAgent.receive(tpl);
             if (msg != null) {
@@ -207,21 +208,25 @@ public class GeneralAgent extends AbstractDedaleAgent {
                 if (content != null) {
                 	boolean m = moveTo(content);
                 	List <Couple<String, List <Couple<Observation, Integer>>>> ob = observe();
-                	
-                	if (type.equals("agentCollect") && sumFreeSpace(getBackPackFreeSpace()) > 0) 
+
+                	Integer fp = sumFreeSpace(getBackPackFreeSpace());
+                	if (type.equals("agentCollect") && fp > 0)
 	                	for (Couple<String, List <Couple<Observation, Integer>>> o : ob)
 	                		if (content.equals(o.getLeft())) {
 	                			handle_current_node(o);
 	                			break;
 	                		}
-                	
+
+                	fp = sumFreeSpace(getBackPackFreeSpace());
+
                 	System.out.println("Observations: " + ob.toString());
-                	
+
                 	Map <String, Object> pass_info = new HashMap <String, Object>();
                 	pass_info.put("OBSERVATIONS", ob);
                 	pass_info.put("CAN_MOVE", m);
                 	pass_info.put("CURRENT_POSITION", getCurrentPosition());
-                	
+                	if (type.equals("agentCollect")) pass_info.put("BACKPACK_SPACE", fp);
+
                 	ACLMessage reply = msg.createReply();
                     reply.setPerformative(ACLMessage.INFORM);
                     try {
@@ -233,7 +238,7 @@ public class GeneralAgent extends AbstractDedaleAgent {
             else { block(); }
         }
 	}
-	
+
 	public Integer sumFreeSpace(List<Couple<Observation, Integer>> bp) {
 		Integer sum = 0;
 		for (Couple<Observation, Integer> b : bp) {
